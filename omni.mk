a392c2144faa94c580df2ee364788fc4b1c7593a
J0SH1X <aljoshua.hell@gmail.com>
2017-06-25 15:59:13 +0200

    Update omni.mk

--- omni.mk
+++ omni.mk
@@ -2,14 +2,14 @@
 PRODUCT_RELEASE_NAME := h870
 
 # Inherit some common AOKP stuff.
-$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/h870/device_h870.mk)
+$(call inherit-product, device/lge/h870/omni_h870.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := h870
-PRODUCT_NAME := aokp_h870
+PRODUCT_NAME := omni_h870
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := h870
 PRODUCT_MANUFACTURER := lge
