7bf32277e1ae95e8b985338760697cd8928b8da9
Aljoshua Hell <aljoshua.hell@gmail.com>
2017-06-27 11:26:34 +0200

    Enable logcat on TWRP and set power button to select

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_BOOTLOADER_BOARD_NAME := h870
 #Try to enable gpu support
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno530
-
+TARGET_HAS_NO_SELECT_BUTTON := true
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff androidboot.hardware=lucye androidboot.bootdevice=624000.ufshc androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
@@ -65,10 +65,7 @@ TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 149
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INCLUDE_NTFS_3G := true
-
-# No love for the wicked (device ships with M)
 TW_EXCLUDE_SUPERSU := true
-
 # Encryption support
 TW_INCLUDE_CRYPTO := true
 #TARGET_HW_DISK_ENCRYPTION := true
@@ -77,3 +74,4 @@ TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
 
 # Debug flags
 TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
